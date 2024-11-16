316e21099df1306a7b66faf67a32b548920de750
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2024-11-16 20:32:24 +0600

    Removing home

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home Page'), // Updated title
+      home: const MyHomePage(), // Removed title
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -42,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        // Removed title
       ),
       body: Center(
         child: Column(
