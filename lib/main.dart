ee15c1bfb12227c67ab5d35c4e3df4d269d2a4a6
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2024-11-16 20:27:48 +0600

    Removing Flutter demo

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Flutter App', // Updated title
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      debugShowCheckedModeBanner: false, // Add this line
+      home: const MyHomePage(title: 'Home Page'), // Updated title
+      debugShowCheckedModeBanner: false,
     );
   }
 }
