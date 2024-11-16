1e497b7a34304298ef4780578d1074bb7b774f93
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2024-11-16 20:34:15 +0600

    Removing banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter App', // Updated title
+      title: 'My Flutter App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(), // Removed title
+      home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -38,10 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Removed title
-      ),
+      // Removed AppBar
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
